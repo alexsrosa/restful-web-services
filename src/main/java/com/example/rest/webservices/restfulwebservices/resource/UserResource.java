6985e572403c3e03724883cc07ccf5a0be0c4c92
Alex S. Rosa <alexsros@gmail.com>
2018-06-02 00:36:23 -0300

    Impl HATEOAS

--- src/main/java/com/example/rest/webservices/restfulwebservices/resource/UserResource.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/resource/UserResource.java
@@ -4,6 +4,10 @@ import com.example.rest.webservices.restfulwebservices.bean.User;
 import com.example.rest.webservices.restfulwebservices.dao.UserDaoService;
 import com.example.rest.webservices.restfulwebservices.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -25,14 +29,18 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public Resource<User> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
 
         if (Objects.isNull(user)){
             throw new UserNotFoundException("id:".concat(String.valueOf(id)));
         }
 
-        return user;
+        Resource<User> resource = new Resource<>(user);
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        resource.add(linkTo.withRel("all-users"));
+
+        return resource;
     }
 
     @DeleteMapping("/users/{id}")
