be0c4adc70b9b4693d11e1bbea1e59e29302f978
Alex S. Rosa <alexsros@gmail.com>
2018-06-02 11:34:37 -0300

    internationalization 1

--- src/main/java/com/example/rest/webservices/restfulwebservices/resource/HelloWorldController.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/resource/HelloWorldController.java
@@ -1,17 +1,35 @@
 package com.example.rest.webservices.restfulwebservices.resource;
 
 import com.example.rest.webservices.restfulwebservices.bean.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    @Autowired
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
 
         return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-internationatized")
+    public String helloWorldInternationatized(@RequestHeader(name = "Accept-Language", required = false) Locale locale){
+
+        return messageSource.getMessage("good.morning.message", null, locale);
     }
 
     @GetMapping(path = "/hello-world-bean")
